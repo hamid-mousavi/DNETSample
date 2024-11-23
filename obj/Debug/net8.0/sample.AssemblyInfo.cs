83d4992e7c9701b6ee4370abe5063dd4bf036693
iR <hamidabadani63@gmail.com>
2024-11-23 12:36:45 +0330

    fix port

--- obj/Debug/net8.0/sample.AssemblyInfo.cs
+++ obj/Debug/net8.0/sample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090c00c5c1ed34c84ecdda31026a3bfda8d67a82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0acc5cae475d89892c1ac48cf03bb11243de0aa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("sample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
